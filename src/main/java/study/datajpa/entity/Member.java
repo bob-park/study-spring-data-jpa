7fcf31123c22381acdefa6f94604a8847c86c308
phw11 <hwpark11@kakao.com>
2021-08-31 16:13:38 +0900

    feat : initialize Project

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,76 @@
+package study.datajpa.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "member_id")
+  private Long id;
+
+  private String username;
+  private int age;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "team_id")
+  private Team team;
+
+  protected Member() {}
+
+  public Member(String username) {
+    this.username = username;
+  }
+
+  public Member(String username, int age, Team team) {
+    this.username = username;
+    this.age = age;
+
+    if (team != null) {
+      changeTeam(team);
+    }
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public Team getTeam() {
+    return team;
+  }
+
+  public void setTeam(Team team) {
+    this.team = team;
+  }
+
+  public void changeTeam(Team team) {
+    this.team = team;
+    team.getMembers().add(this);
+  }
+
+  @Override
+  public String toString() {
+    return "Member{" + "id=" + id + ", username='" + username + ", age=" + age + '}';
+  }
+}
---
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,69 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+  @Autowired private MemberJpaRepository memberJpaRepository;
+
+  @Test
+  void testMember() {
+    // given
+    Member member = new Member("memberA");
+    Member savedMember = memberJpaRepository.save(member);
+
+    // when
+    Member findMember = memberJpaRepository.find(savedMember.getId());
+
+    // then
+    assertThat(findMember.getId()).isEqualTo(member.getId());
+    assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    assertThat(findMember).isEqualTo(member);
+  }
+
+  @Test
+  void basicCRUD() throws Exception {
+    // given
+    Member member1 = new Member("member1");
+    Member member2 = new Member("member2");
+
+    memberJpaRepository.save(member1);
+    memberJpaRepository.save(member2);
+
+    // when
+
+    Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+    Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+    // then
+
+    assertThat(findMember1).isEqualTo(member1);
+    assertThat(findMember2).isEqualTo(member2);
+
+    findMember1.setUsername("member1!!!");
+
+    List<Member> all = memberJpaRepository.findAll();
+    assertThat(all.size()).isEqualTo(2);
+
+    long count = memberJpaRepository.count();
+    assertThat(count).isEqualTo(2);
+
+    memberJpaRepository.delete(member1);
+    memberJpaRepository.delete(member2);
+
+    long deletedCount = memberJpaRepository.count();
+    assertThat(deletedCount).isZero();
+  }
+}
